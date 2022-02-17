53807e6202019e89be777ae35ef79cb19e55b34d
Eric Chomette <ericchomette@Erics-MBP.lan>
2022-02-17 05:55:46 +0100

    add name to user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy ]
-
+  before_action :set_article, only: %i[show]
+  before_action :authenticate_user!
+  before_action :current_user, only: %i[edit update destroy ]
   # GET /articles or /articles.json
   def index
     @articles = Article.all
