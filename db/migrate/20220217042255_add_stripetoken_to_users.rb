b345d2a123926fc6f5031f39de5b339e7b3c4a7d
Eric Chomette <ericchomette@Erics-MBP.lan>
2022-02-17 05:24:45 +0100

    save configuration usersModel

---
+++ db/migrate/20220217042255_add_stripetoken_to_users.rb
@@ -0,0 +1,5 @@
+class AddStripetokenToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :stripetoken, :string
+  end
+end
---
+++ db/migrate/20220217042404_add_birthdate_to_users.rb
@@ -0,0 +1,5 @@
+class AddBirthdateToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :birthdate, :string
+  end
+end
