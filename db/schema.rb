0ae7dd835c3c83ce8ad27ef406f4c346198bf923
Eric Chomette <ericchomette@Erics-MBP.lan>
2022-02-17 05:44:15 +0100

    belong et has many

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_17_042404) do
+ActiveRecord::Schema.define(version: 2022_02_17_043258) do
+
+  create_table "articles", force: :cascade do |t|
+    t.boolean "video"
+    t.boolean "image"
+    t.integer "price"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
